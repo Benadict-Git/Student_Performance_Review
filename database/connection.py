8eac2697044836c824adf68112e4c631fcd63837
Benadict-Git <benadictinfantofficial@gmail.com>
2025-01-07 20:14:45 +0530

    Need to update

--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 def get_db():
-    client = MongoClient("mongodb://localhost:27017/")  # MongoDB URI
+    client = MongoClient("mongodb://localhost:27017/")  # Replace with your MongoDB URI
     db = client.student_performance
     return db
