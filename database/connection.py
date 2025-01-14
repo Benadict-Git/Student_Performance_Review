edf3414c50e9f6e5e44888a8cfa0e30d4b2927cd
Benadict-Git <benadictinfantofficial@gmail.com>
2025-01-14 20:39:29 +0530

    Database connection (MangoDB)

--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 def get_db():
-    client = MongoClient("mongodb://localhost:27017/")  # Replace with your MongoDB URI
+    client = MongoClient("mongodb://localhost:27017/")  # Replace with your MongoDB URI (ATLAS cloud can be used)
     db = client.student_performance
     return db
